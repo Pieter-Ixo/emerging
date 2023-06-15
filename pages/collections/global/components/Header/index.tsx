1122e381eb512b66b983745965f199b44aed24d4
Serhii Perehonchuk <serhii.perehonchuk@sigma.software>
2023-06-15 12:31:49 +0300

    chore: refactored mantine usage with using less custom css

--- pages/collections/global/components/Header/index.tsx
+++ pages/collections/global/components/Header/index.tsx
@@ -1,41 +1,41 @@
-import { Container, Flex, Text, TextInput } from "@mantine/core";
+import { Container, Flex, Text, TextInput, Title } from "@mantine/core";
 
 import PortfolioIcon from "@/components/icons/portfolio-icon";
 import GlobalIcon from "@/components/icons/global-icon";
 import Link from "next/link";
+
 import SearchGlass from "@/assets/icons/search-glass.svg";
 import FilterSettings from "@/assets/icons/filter-settings.svg";
 
 export default function Header() {
   return (
     <Container fluid sx={{ width: "100%" }}>
-      <Flex align="center" gap={24} sx={{ padding: 16, paddingLeft: 32 }}>
+      <Flex align="center" gap={24} pl={32} p={16}>
         <Flex align="center" gap={16}>
           <GlobalIcon selected />
           <Link href="/collections/portfolio">
             <PortfolioIcon />
           </Link>
         </Flex>
-        <Text fw={300} sx={{ fontSize: 40 }}>
+        <Title order={1} fw={300} size="40px">
           Collections
-        </Text>
+        </Title>
         <TextInput
           label=""
           variant="unstyled"
           radius="xl"
           placeholder="search"
           icon={<SearchGlass />}
-          sx={{ backgroundColor: "#fff", borderRadius: "2rem" }}
+          bg="#fff"
+          sx={{ borderRadius: "2rem" }}
         />
         <Flex
           align="center"
           gap={10}
           px="20px"
-          sx={{
-            backgroundColor: "#fff",
-            height: 36,
-            borderRadius: "2rem",
-          }}
+          bg="#fff"
+          h={36}
+          sx={{ borderRadius: "2rem" }}
         >
           <FilterSettings />
           <Text>Filter</Text>
--- pages/collections/portfolio/components/Header/index.tsx
+++ pages/collections/portfolio/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { Container, Flex, Text } from "@mantine/core";
+import { Container, Flex, Text, Title } from "@mantine/core";
 
 import PortfolioIcon from "@/components/icons/portfolio-icon";
 import GlobalIcon from "@/components/icons/global-icon";
@@ -7,16 +7,16 @@ import Link from "next/link";
 export default function Header() {
   return (
     <Container fluid sx={{ width: "100%" }}>
-      <Flex align="center" gap={24} sx={{ padding: 16, paddingLeft: 32 }}>
+      <Flex align="center" gap={24} pl={32} p={16}>
         <Flex align="center" gap={16}>
           <Link href="/collections/global">
             <GlobalIcon />
           </Link>
           <PortfolioIcon selected />
         </Flex>
-        <Text fw={300} sx={{ fontSize: 40 }}>
+        <Title order={1} fw={300} size="40px">
           My Portfolio
-        </Text>
+        </Title>
         <Text>🔴 disconnected</Text>
       </Flex>
     </Container>
