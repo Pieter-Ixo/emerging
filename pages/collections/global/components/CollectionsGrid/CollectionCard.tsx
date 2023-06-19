a7257bc0809868c86a09737753f56daac23a3307
Serhii Perehonchuk <serhii.perehonchuk@sigma.software>
2023-06-19 15:56:30 +0300

    feature: styled Collection Card

--- pages/collections/global/components/CollectionsGrid/CollectionCard.tsx
+++ pages/collections/global/components/CollectionsGrid/CollectionCard.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, Paper, Image } from "@mantine/core";
+import { Box, Text, Avatar, BackgroundImage, Title, Flex } from "@mantine/core";
 
 import styles from "@/styles/pages/collections/CollectionCard.module.scss";
 import { ICollectionExtended } from "@/types/entityCollections";
@@ -6,19 +6,37 @@ import { ICollectionExtended } from "@/types/entityCollections";
 type Props = { collection: ICollectionExtended };
 
 export default function CollectionCard({ collection }: Props) {
-  if (collection._profile === undefined) return null;
+  if (!collection?._profile) return null;
 
   const { brand, name, imageUrl, logoUrl } = collection._profile;
 
   return (
-    <Paper shadow="md" radius="md" p="md" m="lg" withBorder>
-      <Box className={styles.CollectionCard}>
-        <Text>I&apos;m a collection</Text>
-        <Text>{brand}</Text>
-        <Text>{name}</Text>
-        {imageUrl && <Image src={imageUrl} alt="" />}
-        {logoUrl && <Image src={logoUrl} alt="" />}
-      </Box>
-    </Paper>
+    <Box className={styles.CollectionCard} m="lg">
+      <BackgroundImage src={imageUrl || ""} p="md" mih="260px" radius="lg">
+        <Flex justify="space-between" align="flex-end" mih="inherit">
+          <Box>
+            <Title
+              order={3}
+              c="white"
+              weight={700}
+              size="24px"
+              fs="normal"
+              mb="6px"
+            >
+              {brand}
+            </Title>
+            <Text c="white" weight={500} size="14px" fs="normal">
+              {name}
+            </Text>
+          </Box>
+          <Avatar
+            src={logoUrl || ""}
+            alt="Collection Logotype"
+            bg="white"
+            radius="50%"
+          />
+        </Flex>
+      </BackgroundImage>
+    </Box>
   );
 }
