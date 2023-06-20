967d83bd66da0ff8843817b5ce6e418796682fe5
Serhii Perehonchuk <serhii.perehonchuk@sigma.software>
2023-06-20 10:02:48 +0300

    feature: fixed type error in entityCollections/thunks

--- helpers/getCollectionTags.ts
+++ helpers/getCollectionTags.ts
@@ -1,16 +1,16 @@
 import { request } from "@/requests/request";
-import { ICollection, ICollectionProfile } from "@/types/entityCollections";
+import { ICollection, ICollectionTags } from "@/types/entityCollections";
 import getFullServiceEndpoint from "@/utils/getServiceEndpoint";
 
 export default async function getCollectionProfile(
   collection: ICollection
-): Promise<ICollectionProfile | undefined> {
+): Promise<ICollectionTags | undefined> {
   const tagsEndpointLastPart = collection.settings.Tags.serviceEndpoint;
   const collectionTagsEndpoint = getFullServiceEndpoint(
     tagsEndpointLastPart,
     collection.service
   );
-  const tags = await request<ICollectionProfile>(collectionTagsEndpoint);
+  const tags = await request<ICollectionTags>(collectionTagsEndpoint);
 
   if (!tags) throw new Error("Panica!");
 
