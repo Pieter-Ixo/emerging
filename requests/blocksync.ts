553c129548c9cd1e284747c6b88efcf1a754cd27
Serhii Perehonchuk <serhii.perehonchuk@sigma.software>
2023-07-11 19:28:40 +0300

    fix: removed mocks, now real data

--- requests/blocksync.ts
+++ requests/blocksync.ts
@@ -60,32 +60,8 @@ export async function requestBatchByID(
   batchId: IBatch["id"]
 ): Promise<IBatch | undefined> {
   const url = `/api/token/id/${batchId}`;
+
   const { data, problem } = await blocksynkAPI.get<IBatch>(url);
   if (!problem && data) return data;
-  return {
-    id: "998db61979e94a939cfaa635ba8c63d3",
-    index: "bafkreibzfmpb5vi3dezygipylystbunhg5nbqwgdahmf4orgeemitelxae",
-    name: "CARBON",
-    collection: "did:ixo:entity:eb98bb2c92a62557b6c88c6f80e8d258",
-    tokenData: [
-      {
-        aid: 191,
-        uri: "https://ipfs.io/ipfs/bafkreibzfmpb5vi3dezygipylystbunhg5nbqwgdahmf4orgeemitelxae",
-        encrypted: false,
-        proof: "bafkreibzfmpb5vi3dezygipylystbunhg5nbqwgdahmf4orgeemitelxae",
-        type: "application/json",
-        id: "urn:uuid:bd7413dc-003a-4b9f-bfdf-e4cf32dd7a66",
-        tokenId: "998db61979e94a939cfaa635ba8c63d3",
-      },
-      {
-        aid: 192,
-        uri: "",
-        encrypted: false,
-        proof: "",
-        type: "application/json",
-        id: "did:ixo:entity:fe10ed6f17aa11f35068b126be43a1fc",
-        tokenId: "998db61979e94a939cfaa635ba8c63d3",
-      },
-    ],
-  };
+  throw new Error("no batch for this id");
 }
