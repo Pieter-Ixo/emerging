20513888e94b47541f206004385277a5e4e19796
Serhii Perehonchuk <serhii.perehonchuk@sigma.software>
2023-07-18 16:44:31 +0300

    refactor/e-105: card-performance now simple and using mantine

---
+++ components/card-performance/TabButton.tsx
@@ -0,0 +1,43 @@
+import { ElementType } from "react";
+import { Text, Box, Flex, UnstyledButton } from "@mantine/core";
+
+import styles from "./card-performance.module.scss";
+
+export default function TabButton({
+  name,
+  isActive,
+  onClick,
+  Icon,
+}: {
+  name: string;
+  isActive: boolean;
+  onClick: () => void;
+  Icon: ElementType;
+}) {
+  return (
+    <UnstyledButton
+      variant="subtle"
+      h="unset"
+      p={0}
+      className={styles.section}
+      onClick={onClick}
+    >
+      <Flex direction="column" align="center">
+        <Box
+          color="none"
+          sx={{
+            borderRadius: "50%",
+            background: isActive ? "#ffffff" : "transparent",
+            width: 42,
+            height: 42,
+            textAlign: "center",
+            paddingTop: "5px",
+          }}
+        >
+          <Icon width={32} height={32} />
+        </Box>
+        <Text>{name.toUpperCase()}</Text>
+      </Flex>
+    </UnstyledButton>
+  );
+}
