a3f59053de48771b9234f3d6625ab2bb8ab0947c
Serhii Perehonchuk <serhii.perehonchuk@sigma.software>
2023-07-27 14:13:23 +0300

    Merge remote-tracking branch 'origin/fix/e-116/layout-refactor'

--- pages/entity/[entityId]/batch/components/Header/index.tsx
+++ pages/entity/[entityId]/batch/components/Header/index.tsx
@@ -1,19 +1,12 @@
-import GlobalIcon from "@/components/icons/global-icon";
-import PortfolioIcon from "@/components/icons/portfolio-icon";
+import HeaderControls from "@/components/HeaderControls";
 import { Container, Flex, Title } from "@mantine/core";
-import Link from "next/link";
 import BatchesControls from "../BatchesControls/BatchesControls";
 
 export default function Header() {
   return (
     <Container fluid mb="xl" px="0" sx={{ width: "100%" }}>
       <Flex align="center" gap={24}>
-        <Flex align="center" gap={16}>
-          <Link href="/collections/global">
-            <GlobalIcon />
-          </Link>
-          <PortfolioIcon selected />
-        </Flex>
+      <HeaderControls selectedLink="portfolio"/>
         <Title order={1} fw={300} size="40px">
           Carbon Certificates
         </Title>
