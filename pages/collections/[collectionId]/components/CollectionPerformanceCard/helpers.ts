b5dda8172b2d0cba2343272883c2d9c761c532a9
Serhii Perehonchuk <serhii.perehonchuk@sigma.software>
2023-07-31 13:39:43 +0300

    feat/e-118: added row which shows total sessions

--- pages/collections/[collectionId]/components/CollectionPerformanceCard/helpers.ts
+++ pages/collections/[collectionId]/components/CollectionPerformanceCard/helpers.ts
@@ -21,3 +21,13 @@ export function summaryToChartData(summary: Record<string, number>) {
   }, []);
   return data;
 }
+
+export function calculateTotalSessions(
+  summary: MONTH_SESSIONS_TOTAL_MAP
+): number {
+  return Object.values(summary).reduce(
+    (acc, device) =>
+      acc + Object.values(device).reduce((acc2, month) => acc2 + month, 0),
+    0
+  );
+}
--- pages/collections/[collectionId]/components/CollectionPerformanceCard/index.tsx
+++ pages/collections/[collectionId]/components/CollectionPerformanceCard/index.tsx
@@ -1,12 +1,14 @@
 import { useEffect } from "react";
-import { Loader, Text } from "@mantine/core";
+import { Flex, Loader, Text } from "@mantine/core";
 
 import { useAppSelector } from "@/hooks/redux";
 import { useCookstove } from "@/context/cookstove";
 import { selectAllEntitiesExternalIds } from "@/redux/entityCollections/selectors";
+import { palette } from "@/theme/palette";
 
 import PageBlock from "../PageBlock";
 import SessionsChart from "./SessionsChart";
+import { calculateTotalSessions } from "./helpers";
 
 export default function CollectionPerformanceCard() {
   const {
@@ -20,10 +22,25 @@ export default function CollectionPerformanceCard() {
     fetchMonthSummary(entitesExternalIds);
   }, []);
 
+  const totalValue = sessionsSummary
+    ? calculateTotalSessions(sessionsSummary)
+    : 0;
+
   return (
     <PageBlock title="COLLECTION PERFORMANCE">
       {sessionsSummary ? (
-        <SessionsChart sessionsSummary={sessionsSummary} />
+        <>
+          <Flex pt={28} align="flex-end">
+            <Text size={56} color={palette.fullBlue} pr={10} fs="normal">
+              {totalValue.toLocaleString()}
+            </Text>
+            <Text color={palette.Black} pb={18} fs="normal" weight={300}>
+              clean cooking sessions with renewable energy
+            </Text>
+          </Flex>
+
+          <SessionsChart sessionsSummary={sessionsSummary} />
+        </>
       ) : (
         <Loader />
       )}
