18c79e373bf6d59967dec3ed42411f213b4ffbb2
Serhii Perehonchuk <serhii.perehonchuk@sigma.software>
2023-08-03 12:55:35 +0300

    chore/e-XXX: deleted unused files with npx unimported

--- helpers/byKey.ts
+++
@@ -1,5 +0,0 @@
-export default function byKey(
-  keyName: string
-): (a: Record<string, any>) => boolean {
-  return (a) => a.key === keyName;
-}
--- helpers/transformData/getCollectionsDataFromResponse.ts
+++
@@ -1,7 +0,0 @@
-import { IApiEntityCollectionsResponse, ICollection } from "@/types/entityCollections";
-
-export default function getCollectionsDataFromResponse(
-  responseData: IApiEntityCollectionsResponse
-): ICollection[] {
-  return responseData?.map((item) => item.collection);
-}
