7d0eba67a803a7c7c2d244d42e583fb9dbf7caed
Serhii Perehonchuk <serhii.perehonchuk@sigma.software>
2023-08-08 11:37:44 +0300

    feat/e-110: portfolio tab disabled unless logged in

--- components/Layout/GlobalPortfolioSwitch/index.tsx
+++ components/Layout/GlobalPortfolioSwitch/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import PortfolioIcon from "@/icons/portfolio-icon";
 import GlobalIcon from "@/icons/global-icon";
+import { useAppSelector } from "@/hooks/redux";
 
 type Props = {
   selectedLink: "global" | "portfolio";
@@ -11,16 +12,25 @@ type Props = {
 export default function GlobalPortfolioSwitch({
   selectedLink = "global",
 }: Props) {
+  const userAddress = useAppSelector((state) => state.user.connectedWallet);
   const isGlobalSelected = selectedLink === "global";
   const isPortfolioSelected = selectedLink === "portfolio";
 
+  const portfolioLinkStatus = !userAddress
+    ? "disabled"
+    : isPortfolioSelected
+    ? "active"
+    : "inactive";
   return (
     <Flex align="center" gap={16}>
       <Link href="/collections/global">
         <GlobalIcon selected={isGlobalSelected} />
       </Link>
-      <Link href="/collections/portfolio">
-        <PortfolioIcon selected={isPortfolioSelected} />
+      <Link
+        href="/collections/portfolio"
+        style={{ pointerEvents: userAddress ? "unset" : "none" }}
+      >
+        <PortfolioIcon status={portfolioLinkStatus} />
       </Link>
     </Flex>
   );
