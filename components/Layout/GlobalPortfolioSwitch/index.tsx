3f77d5599dae64e1d60147c05f80b388ffd6b9d0
Serhii Perehonchuk <serhii.perehonchuk@sigma.software>
2023-08-08 12:41:17 +0300

    feat/e-110: portfolio tab states now better managed

--- components/Layout/GlobalPortfolioSwitch/index.tsx
+++ components/Layout/GlobalPortfolioSwitch/index.tsx
@@ -9,29 +9,33 @@ type Props = {
   selectedLink: "global" | "portfolio";
 };
 
+function PortfolioLink({ isSelected }: { isSelected: boolean }) {
+  const userAddress = useAppSelector((state) => state.user.connectedWallet);
+  if (!userAddress)
+    return (
+      <Link href="/collections/portfolio" style={{ pointerEvents: "none" }}>
+        <PortfolioIcon status="disabled" />
+      </Link>
+    );
+  return (
+    <Link href="/collections/portfolio">
+      <PortfolioIcon status={isSelected ? "selected" : "notSelected"} />
+    </Link>
+  );
+}
+
 export default function GlobalPortfolioSwitch({
   selectedLink = "global",
 }: Props) {
-  const userAddress = useAppSelector((state) => state.user.connectedWallet);
   const isGlobalSelected = selectedLink === "global";
   const isPortfolioSelected = selectedLink === "portfolio";
 
-  const portfolioLinkStatus = !userAddress
-    ? "disabled"
-    : isPortfolioSelected
-    ? "active"
-    : "inactive";
   return (
     <Flex align="center" gap={16}>
       <Link href="/collections/global">
         <GlobalIcon selected={isGlobalSelected} />
       </Link>
-      <Link
-        href="/collections/portfolio"
-        style={{ pointerEvents: userAddress ? "unset" : "none" }}
-      >
-        <PortfolioIcon status={portfolioLinkStatus} />
-      </Link>
+      <PortfolioLink isSelected={isPortfolioSelected} />
     </Flex>
   );
 }
