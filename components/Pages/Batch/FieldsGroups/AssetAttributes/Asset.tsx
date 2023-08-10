cf41b661600a1dc6b22a3ddae60db2a77cf27fbc
VerseVent <mike.82pronka.27a@gmail.com>
2023-08-10 15:31:04 +0200

    feat/e-42: add example buttons for verify layout

--- components/Pages/Batch/FieldsGroups/AssetAttributes/Asset.tsx
+++ components/Pages/Batch/FieldsGroups/AssetAttributes/Asset.tsx
@@ -1,9 +1,10 @@
-import { Card, Flex, Button, Text } from "@mantine/core";
+import { Card, Flex, Button, Text, Box } from "@mantine/core";
 
 import JSONViewer from "@/components/Presentational/JSONViewer";
 import useDetailPortal from "@/hooks/useDetailPortal";
 import { palette } from "@/theme/palette";
 
+import VerifyIcon from "@/icons/batches/VerifyIcon";
 import { FieldText } from "../styledComponents";
 import { AssetAttributesProps } from "./props";
 
@@ -14,18 +15,38 @@ export default function AssetName({
   const { isVisible, openPortal, closePortal } = useDetailPortal("AssetName");
 
   const PortalChild = (
-    <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Flex
-        align="center"
-        justify="space-between"
-        sx={{ borderBottom: `1px solid ${palette.Black}` }}
-        mb="xs"
+    <Box w={322}>
+      <Card shadow="sm" padding="lg" radius="md" withBorder>
+        <Flex
+          align="center"
+          justify="space-between"
+          sx={{ borderBottom: `1px solid ${palette.Black}` }}
+          mb="xs"
+        >
+          <Text>Asset Credential Subject</Text>
+          <Text>DOWNLOAD ➔</Text>
+        </Flex>
+        <JSONViewer json={JSON.stringify(deviceCredSubject)} />
+      </Card>
+      <Button
+        color="yellow"
+        mt={40}
+        mb={16}
+        h={46}
+        w="100%"
+        radius="xl"
+        leftIcon={<VerifyIcon />}
       >
-        <Text>Asset Credential Subject</Text>
-        <Text>DOWNLOAD ➔</Text>
-      </Flex>
-      <JSONViewer json={JSON.stringify(deviceCredSubject)} />
-    </Card>
+        <Text fw={400} size={16}>
+          Unverified
+        </Text>
+      </Button>
+      <Button h={46} w="100%" radius="xl" leftIcon={<VerifyIcon />}>
+        <Text fw={400} size={16}>
+          Download
+        </Text>
+      </Button>
+    </Box>
   );
 
   return (
