000726f5df75a09bf0afdb629963a30d5b510cb7
VerseVent <mike.82pronka.27a@gmail.com>
2023-08-21 17:25:36 +0200

    feat/e-72: change mocked address to CapeTown

--- components/Pages/Collections/CollectionDashboard/CollectionPerformanceCard/CollectionLocation/index.tsx
+++ components/Pages/Collections/CollectionDashboard/CollectionPerformanceCard/CollectionLocation/index.tsx
@@ -13,7 +13,7 @@ export default function CollectionLocation() {
   return collection?._profile?.location ? (
     <iframe
       title="Collection geolocation"
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2990.274257380938!2d-70.56068388481569!3d41.45496659976631!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e52963ac45bbcb%3A0xf05e8d125e82af10!2sDos%20Mas!5e0!3m2!1sen!2sus!4v1671220374408!5m2!1sen!2sus"
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d423812.3292600542!2d18.3264235053273!3d-33.914529073531575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc500f8826eed7%3A0x687fe1fc2828aa87!2sCape%20Town%2C%20South%20Africa!5e0!3m2!1sen!2sus!4v1692631487951!5m2!1sen!2sus"
       style={{
         width: "100%",
         height: 287,
