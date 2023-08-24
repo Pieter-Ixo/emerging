017dd111b9f77f6ebb0aa99c68c11db22cb03416
VerseVent <mike.82pronka.27a@gmail.com>
2023-08-24 14:44:35 +0200

    feat/e-207: change design of the collection card

--- components/Pages/Collections/Global/CollectionsGrid/CollectionCard.tsx
+++ components/Pages/Collections/Global/CollectionsGrid/CollectionCard.tsx
@@ -1,61 +1,92 @@
 import { useRouter } from "next/router";
-import { Box, Text, Avatar, BackgroundImage, Title, Flex } from "@mantine/core";
+import { useEffect } from "react";
+import {
+  Box,
+  Text,
+  Avatar,
+  Flex,
+  Card,
+  BackgroundImage,
+  Badge,
+} from "@mantine/core";
 
+import { useAppDispatch, useAppSelector } from "@/hooks/redux";
 import { ICollectionExtended } from "@/types/entityCollections";
 import getEntityTagsByCategory from "@/helpers/transformData/getEntityTagsByCategory";
+import { palette } from "@/theme/palette";
+import { fetchCollectionTokenIpfs } from "@/redux/entityCollections/thunks";
+import { selectCollectionsTokensIpfs } from "@/redux/entityCollections/selectors";
 
 import TagIcon from "./TagIcon";
 
-type Props = { collection: ICollectionExtended };
+type Props = { collection: ICollectionExtended; entitiesLength: number };
 
-export default function CollectionCard({ collection }: Props) {
+export default function CollectionCard({ collection, entitiesLength }: Props) {
+  const dispatch = useAppDispatch();
   const router = useRouter();
+  const collectionTokenIpfs = useAppSelector((state) =>
+    selectCollectionsTokensIpfs(state, collection.id)
+  );
+
+  useEffect(() => {
+    if (!collection._tokenIpfs) dispatch(fetchCollectionTokenIpfs(collection));
+  }, [collection.id]);
+
   if (!collection?._profile) return null;
 
   const { brand, name, imageUrl, logoUrl } = collection._profile;
 
   const tags = getEntityTagsByCategory(collection, "SDG") ?? [];
 
+  const badgeTitle = collectionTokenIpfs?.properties.denom;
+
   return (
-    <BackgroundImage
-      src={imageUrl || ""}
+    <Card
+      onClick={() => router.push(`/collections/${collection.id}`)}
       sx={{ cursor: "pointer" }}
-      p="md"
       maw="400px"
       mih="260px"
+      padding={0}
       radius="lg"
-      onClick={() => router.push(`/collections/${collection.id}`)}
     >
-      <Flex direction="column" mih="inherit" justify="space-between">
-        <Flex justify="flex-end" align="flex-start" gap="10px">
-          {tags.map((tagText) => (
-            <TagIcon name={tagText} key={tagText} />
-          ))}
-        </Flex>
-        <Flex justify="space-between" align="flex-end">
-          <Box>
-            <Title
-              order={4}
-              c="white"
-              weight={700}
-              size="24px"
-              fs="normal"
-              mb="6px"
-            >
-              {brand}
-            </Title>
-            <Text c="white" weight={500} size="14px" fs="normal">
-              {name}
-            </Text>
-          </Box>
+      <Card.Section>
+        <BackgroundImage src={imageUrl} mih={250}>
+          <Flex justify="flex-end" align="flex-start" gap="sm" p="md">
+            {tags.map((tagText) => (
+              <TagIcon name={tagText} key={tagText} />
+            ))}
+          </Flex>
+        </BackgroundImage>
+      </Card.Section>
+      <Flex gap="md" justify="space-between" p="sm" bg={palette.Neutral200}>
+        <Box>
+          <Text>{brand}</Text>
+          <Text>{name}</Text>
+        </Box>
+        <Flex align="end" pos="relative">
           <Avatar
+            pos="absolute"
+            top="-60%"
+            left="40%"
             src={logoUrl || ""}
             alt="Collection Logotype"
-            bg="white"
+            bg={palette.White}
             radius="50%"
           />
+          <Flex align="center" gap={8}>
+            <Text size="md">{entitiesLength || 0}</Text>
+            <Badge
+              sx={{ background: palette.greenFull }}
+              radius="md"
+              variant="filled"
+            >
+              <Text size="md" fw={500}>
+                {badgeTitle}
+              </Text>
+            </Badge>
+          </Flex>
         </Flex>
       </Flex>
-    </BackgroundImage>
+    </Card>
   );
 }
