92c75d4e8df17ddcb6ba095e4872d4929c9208d4
VerseVent <mike.82pronka.27a@gmail.com>
2023-09-08 12:59:33 +0200

    feat/e-245: fix bug with routing, block transaction view if user not logged

--- pages/certificate/[transactionId]/index.tsx
+++ pages/certificate/[transactionId]/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { Alert, Loader } from "@mantine/core";
+import { useRouter } from "next/router";
 
 import { useAppDispatch, useAppSelector } from "@/hooks/redux";
 import useValueFromRouter from "@/utils/useValueFromRouter";
@@ -11,21 +12,28 @@ import {
   selectTransactionError,
   selectTransactionIsLoading,
 } from "@/redux/transactions/selectors";
+import { selectConnectedWallet } from "@/redux/selectors";
 import { fetchTransactionByHash } from "@/redux/transactions/thunks";
 import CertificatesGrid from "@/components/Pages/Certificate/CertifcatesGrid";
 
 export default function TransactionCertificate() {
   const dispatch = useAppDispatch();
+  const router = useRouter();
   const transactionId = useValueFromRouter("transactionId");
   const transaction = useAppSelector(selectTransaction);
   const transactionIsLoading = useAppSelector(selectTransactionIsLoading);
   const transactionError = useAppSelector(selectTransactionError);
+  const userWallet = useAppSelector(selectConnectedWallet);
+
   useEffect(() => {
-    if (transactionId) {
+    if (transactionId && userWallet)
       dispatch(fetchTransactionByHash(transactionId));
-    }
   }, [transactionId]);
 
+  useEffect(() => {
+    if (!userWallet)
+      router.push("/collections/global", undefined, { shallow: true });
+  }, []);
   return (
     <AppLayout title="Transaction Certificate">
       <BatchesPageHeader
