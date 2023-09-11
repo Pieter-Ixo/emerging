77b26b58d3a565d8325bdb498db85b8fa03e3c0f
Serhii Perehonchuk <serhii.perehonchuk@sigma.software>
2023-09-11 14:29:18 +0300

    test/e-246: added test grouping

--- e2e-tests/global-collections/list-collections.spec.ts
+++ e2e-tests/global-collections/list-collections.spec.ts
@@ -14,30 +14,21 @@ test("should redirect from '/' to global collections list", async ({
   page,
 }) => {
   const url = await page.url();
-  await expect(url).toEqual(LOCATION_GLOBAL_COLLECTIONS_LIST);
+  expect(url).toEqual(LOCATION_GLOBAL_COLLECTIONS_LIST);
 });
 
-test("should request and display collections", async ({ page }) => {
-  const requestGlobalCollections = await page.waitForRequest(
-    `${BLOCK_SYNC_URL}/api/entity/collections`
-  );
+test.describe("Home page. The global collections list", () => {
+  test("should request collections", async ({ page }) => {
+    const requestGlobalCollections = await page.waitForRequest(
+      `${BLOCK_SYNC_URL}/api/entity/collections`
+    );
+    const requestedURL = requestGlobalCollections.url();
+    expect(requestedURL).toEqual(`${BLOCK_SYNC_URL}/api/entity/collections`);
+  });
 
-  const requestedURL = requestGlobalCollections.url();
-  expect(requestedURL).toEqual(`${BLOCK_SYNC_URL}/api/entity/collections`);
-
-  const CollectionCard = await page.getByTestId("CollectionCard");
-  await page.getByTestId("CollectionCard").waitFor();
-  await expect(CollectionCard).toBeVisible();
+  test("should display collections cards", async ({ page }) => {
+    const CollectionCard = await page.getByTestId("CollectionCard");
+    await page.getByTestId("CollectionCard").waitFor();
+    await expect(CollectionCard).toBeVisible();
+  });
 });
-
-// test("get started link", async ({ page }) => {
-//   await page.goto("https://playwright.dev/");
-
-//   // Click the get started link.
-//   await page.getByRole("link", { name: "Get started" }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(
-//     page.getByRole("heading", { name: "Installation" })
-//   ).toBeVisible();
-// });
